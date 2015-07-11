b66be994d44c8091e0ef7c45e7278f9beaa921fa
Белостоцкий Алексей <lexek-92@ya.ru>
2015-07-11 21:36:56 +0300

    #20 Сделал отдельный сервия для асинхронной нотификации для пользователей при входе в комнату.

--- src/main/java/lexek/wschat/services/TicketService.java
+++ src/main/java/lexek/wschat/services/TicketService.java
@@ -4,16 +4,31 @@ import lexek.wschat.chat.*;
 import lexek.wschat.db.dao.TicketDao;
 import lexek.wschat.db.jooq.tables.pojos.Ticket;
 import lexek.wschat.db.model.DataPage;
+import lexek.wschat.db.model.Email;
 import lexek.wschat.db.model.UserDto;
 import lexek.wschat.db.model.rest.TicketRestModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import java.util.List;
 
 public class TicketService {
+    private final Logger logger = LoggerFactory.getLogger(TicketService.class);
+    private final UserService userService;
+    private final EmailService emailService;
     private final TicketDao dao;
     private final MessageBroadcaster messageBroadcaster;
 
     private int pageLength = 20;
 
-    public TicketService(TicketDao dao, MessageBroadcaster messageBroadcaster) {
+    public TicketService(UserService userService,
+                         EmailService emailService,
+                         TicketDao dao,
+                         MessageBroadcaster messageBroadcaster) {
+        this.userService = userService;
+        this.emailService = emailService;
         this.dao = dao;
         this.messageBroadcaster = messageBroadcaster;
     }
@@ -45,6 +60,18 @@ public class TicketService {
             Message msg = Message.infoMessage("Your ticket " + ticket.getText() + " was closed by " + closedBy.getName() +
                 " with comment: " + comment);
             messageBroadcaster.submitMessage(msg, Connection.STUB_CONNECTION, new UserIdFilter(ticket.getId()));
+            UserDto user = userService.fetchById(ticket.getUser());
+            if (user != null && user.getEmail() != null && user.hasRole(GlobalRole.USER)) {
+                try {
+                    emailService.sendEmail(new Email(
+                        new InternetAddress(user.getEmail()),
+                        "Your ticket is closed",
+                        "Your ticket " + ticket.getText() + " was closed by " + closedBy.getName() +
+                            " with comment: " + comment));
+                } catch (AddressException e) {
+                    logger.warn("error while sending notification email", e);
+                }
+            }
         }
     }
 
@@ -54,6 +81,10 @@ public class TicketService {
 
     public DataPage<Ticket> getAllTicketsForUser(UserDto user, int page) {
         return dao.getAll(user, page, 10);
+    }
+
+    public List<Ticket> getUnreadTickets(UserDto user) {
+        return dao.getNotDeliveredTicketsForUser(user.getId());
     }
 
     public int countOpenTickets() {
