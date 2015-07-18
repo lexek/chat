d3d1ccc932e9a0dd60e7a5f847026e5fafc42243
Белостоцкий Алексей <lexek-92@ya.ru>
2015-07-19 02:47:31 +0300

    #38 Тесты MsgHandler

--- src/main/java/lexek/wschat/chat/handlers/MsgHandler.java
+++ src/main/java/lexek/wschat/chat/handlers/MsgHandler.java
@@ -18,24 +18,22 @@ public class MsgHandler extends AbstractMsgHandler {
 
     @Override
     protected void handle(Connection connection, Room room, Chatter chatter, List<String> args) {
-        if (room != null && room.contains(connection)) {
-            String msg = args.get(1).trim();
-            if (msg.length() == 0) {
-                connection.send(Message.errorMessage("EMPTY_MESSAGE"));
-            } else if ((chatter.hasRole(LocalRole.MOD) || connection.getUser().hasRole(GlobalRole.MOD)) && (msg.length() > 420)) {
-                connection.send(Message.errorMessage("MESSAGE_TOO_BIG"));
-            } else {
-                Message message = Message.msgMessage(
-                    room.getName(),
-                    chatter.getUser().getName(),
-                    chatter.getRole(),
-                    chatter.getUser().getRole(),
-                    chatter.getUser().getColor(),
-                    messageId.getAndIncrement(),
-                    System.currentTimeMillis(),
-                    msg);
-                messageBroadcaster.submitMessage(message, connection, room.FILTER);
-            }
+        String msg = args.get(1).trim();
+        if (msg.length() == 0) {
+            connection.send(Message.errorMessage("EMPTY_MESSAGE"));
+        } else if (!(chatter.hasRole(LocalRole.MOD) || connection.getUser().hasRole(GlobalRole.MOD)) && (msg.length() > 420)) {
+            connection.send(Message.errorMessage("MESSAGE_TOO_BIG"));
+        } else {
+            Message message = Message.msgMessage(
+                room.getName(),
+                chatter.getUser().getName(),
+                chatter.getRole(),
+                chatter.getUser().getRole(),
+                chatter.getUser().getColor(),
+                messageId.getAndIncrement(),
+                System.currentTimeMillis(),
+                msg);
+            messageBroadcaster.submitMessage(message, connection, room.FILTER);
         }
     }
 }
