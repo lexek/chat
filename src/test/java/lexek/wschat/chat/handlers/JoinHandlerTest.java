f451a1b6187a79cf453e5eb2bc37a1c502a9f267
Белостоцкий Алексей <lexek-92@ya.ru>
2015-07-19 01:45:00 +0300

    #38 Тесты JoinHandler

---
+++ src/test/java/lexek/wschat/chat/handlers/JoinHandlerTest.java
@@ -0,0 +1,136 @@
+package lexek.wschat.chat.handlers;
+
+import com.google.common.collect.ImmutableList;
+import lexek.wschat.chat.*;
+import lexek.wschat.db.model.Chatter;
+import lexek.wschat.db.model.UserDto;
+import lexek.wschat.services.RoomJoinNotificationService;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.*;
+
+public class JoinHandlerTest {
+    @Test
+    public void shouldHaveJoinType() {
+        JoinHandler handler = new JoinHandler(null, null, null);
+        assertEquals(handler.getType(), MessageType.JOIN);
+    }
+
+    @Test
+    public void shouldHaveSingleArgument() {
+        JoinHandler handler = new JoinHandler(null, null, null);
+        assertEquals(handler.getArgCount(), 1);
+    }
+
+    @Test
+    public void shouldBeAvailableForAllRoles() {
+        JoinHandler handler = new JoinHandler(null, null, null);
+        assertEquals(handler.getRole(), GlobalRole.UNAUTHENTICATED);
+    }
+
+
+    @Test
+    public void should() {
+        RoomJoinNotificationService roomJoinNotificationService = mock(RoomJoinNotificationService.class);
+        RoomManager roomManager = mock(RoomManager.class);
+        Room room = mock(Room.class);
+        when(roomManager.getRoomInstance("#main")).thenReturn(room);
+        when(roomManager.getRoomInstance(0L)).thenReturn(room);
+        MessageBroadcaster messageBroadcaster = mock(MessageBroadcaster.class);
+        JoinHandler handler = new JoinHandler(roomJoinNotificationService, roomManager, messageBroadcaster);
+
+        UserDto userDto = new UserDto(0L, "user", GlobalRole.USER, "#ffffff", false, false, null);
+        User user = new User(userDto);
+        Connection connection = spy(new TestConnection(user));
+        Chatter chatter = new Chatter(0L, LocalRole.USER, false, null, user);
+
+        when(room.contains(connection)).thenReturn(false);
+        when(room.join(connection)).thenReturn(chatter);
+        when(room.getName()).thenReturn("#main");
+        when(room.hasUser(user)).thenReturn(false);
+
+        handler.handle(ImmutableList.of("#main"), connection);
+
+        verify(room).join(connection);
+        verify(messageBroadcaster).submitMessage(eq(Message.joinMessage("#main", userDto)), eq(connection), eq(room.FILTER));
+        verify(connection).send(eq(Message.selfJoinMessage("#main", chatter)));
+    }
+
+    @Test
+    public void shouldNotBroadcastMessageWhenOtherSessionActive() {
+        RoomJoinNotificationService roomJoinNotificationService = mock(RoomJoinNotificationService.class);
+        RoomManager roomManager = mock(RoomManager.class);
+        Room room = mock(Room.class);
+        when(roomManager.getRoomInstance("#main")).thenReturn(room);
+        when(roomManager.getRoomInstance(0L)).thenReturn(room);
+        MessageBroadcaster messageBroadcaster = mock(MessageBroadcaster.class);
+        JoinHandler handler = new JoinHandler(roomJoinNotificationService, roomManager, messageBroadcaster);
+
+        UserDto userDto = new UserDto(0L, "user", GlobalRole.USER, "#ffffff", false, false, null);
+        User user = new User(userDto);
+        Connection connection = spy(new TestConnection(user));
+        Chatter chatter = new Chatter(0L, LocalRole.USER, false, null, user);
+
+        when(room.contains(connection)).thenReturn(false);
+        when(room.join(connection)).thenReturn(chatter);
+        when(room.getName()).thenReturn("#main");
+        when(room.hasUser(user)).thenReturn(true);
+
+        handler.handle(ImmutableList.of("#main"), connection);
+
+        verify(room).join(connection);
+        verify(messageBroadcaster, never()).submitMessage(any(Message.class), any(Connection.class), any(BroadcastFilter.class));
+        verify(connection).send(eq(Message.selfJoinMessage("#main", chatter)));
+    }
+
+    @Test
+    public void shouldReturnErrorIfAlreadyInRoom() {
+        RoomJoinNotificationService roomJoinNotificationService = mock(RoomJoinNotificationService.class);
+        RoomManager roomManager = mock(RoomManager.class);
+        Room room = mock(Room.class);
+        when(roomManager.getRoomInstance("#main")).thenReturn(room);
+        when(roomManager.getRoomInstance(0L)).thenReturn(room);
+        MessageBroadcaster messageBroadcaster = mock(MessageBroadcaster.class);
+        JoinHandler handler = new JoinHandler(roomJoinNotificationService, roomManager, messageBroadcaster);
+
+        UserDto userDto = new UserDto(0L, "user", GlobalRole.USER, "#ffffff", false, false, null);
+        User user = new User(userDto);
+        Connection connection = spy(new TestConnection(user));
+        Chatter chatter = new Chatter(0L, LocalRole.USER, false, null, user);
+
+        when(room.contains(connection)).thenReturn(true);
+        when(room.join(connection)).thenReturn(chatter);
+        when(room.getName()).thenReturn("#main");
+        when(room.hasUser(user)).thenReturn(true);
+
+        handler.handle(ImmutableList.of("#main"), connection);
+
+        verify(room, never()).join(connection);
+        verify(messageBroadcaster, never()).submitMessage(any(Message.class), any(Connection.class), any(BroadcastFilter.class));
+        verify(connection, never()).send(eq(Message.selfJoinMessage("#main", chatter)));
+        verify(connection).send(eq(Message.errorMessage("ROOM_ALREADY_JOINED")));
+    }
+
+    @Test
+    public void shouldReturnErrorIfNoRoom() {
+        RoomJoinNotificationService roomJoinNotificationService = mock(RoomJoinNotificationService.class);
+        RoomManager roomManager = mock(RoomManager.class);
+        when(roomManager.getRoomInstance("#main")).thenReturn(null);
+        when(roomManager.getRoomInstance(0L)).thenReturn(null);
+        MessageBroadcaster messageBroadcaster = mock(MessageBroadcaster.class);
+        JoinHandler handler = new JoinHandler(roomJoinNotificationService, roomManager, messageBroadcaster);
+
+        UserDto userDto = new UserDto(0L, "user", GlobalRole.USER, "#ffffff", false, false, null);
+        User user = new User(userDto);
+        Connection connection = spy(new TestConnection(user));
+        Chatter chatter = new Chatter(0L, LocalRole.USER, false, null, user);
+
+        handler.handle(ImmutableList.of("#main"), connection);
+
+        verify(messageBroadcaster, never()).submitMessage(any(Message.class), any(Connection.class), any(BroadcastFilter.class));
+        verify(connection, never()).send(eq(Message.selfJoinMessage("#main", chatter)));
+        verify(connection).send(eq(Message.errorMessage("ROOM_NOT_FOUND", ImmutableList.of("#main"))));
+    }
+
+}
