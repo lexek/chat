fc242d3b22cac765fabfd2b3b49c42a25dab3230
Belostotskiy Alexey <lexek92@mail.ru>
2016-02-24 23:08:12 +0300

    #192 slightly decreased twitter avatar margin

--- src/main/resources/static/js/chat/twitter.js
+++ src/main/resources/static/js/chat/twitter.js
@@ -81,7 +81,7 @@ angular.module("chat.twitter", ["pasvaz.bindonce"])
                             'id="::displayTweet.id"/>'+
                     '</div>' +
                     '<twitter-avatar ' +
-                        'class="pull-left" style="margin-right: 5px;" ' +
+                        'class="pull-left" style="margin-right: 2px;" ' +
                         'url="::displayTweet.fromAvatar" ' +
                         'name="::displayTweet.from"' +
                         '></twitter-avatar>' +
