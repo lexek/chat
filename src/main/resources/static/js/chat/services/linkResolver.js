7b9f7bc446c2d5261671044caf7b40a7fb8e9674
Belostotskiy Alexey <lexek92@mail.ru>
2016-03-20 21:51:16 +0300

    YouTube video time display.

--- src/main/resources/static/js/chat/services/linkResolver.js
+++ src/main/resources/static/js/chat/services/linkResolver.js
@@ -7,7 +7,7 @@ module.service("linkResolver", ["$q", "$http", function($q, $http) {
         return "<a href=\"" + prefix + htmlEscape(link) + "\" target=\"_blank\" title=\"" + htmlEscape(link) + "\">" + linkText + "</a>";
     };
 
-    var fetchYoutubeTitle = function(videoId, ytKey, prefix, link, linkText, deferred) {
+    var fetchYoutubeTitle = function(videoId, ytKey, prefix, link, linkText, second, deferred) {
         $http({
             method: 'GET',
             url: 'https://www.googleapis.com/youtube/v3/videos',
@@ -20,6 +20,22 @@ module.service("linkResolver", ["$q", "$http", function($q, $http) {
             if (data.items && data.items[0] && data.items[0].snippet) {
                 var text = "<span style=\"color:#cd201f;\" class=\"fa fa-youtube-play\"></span> "
                     + htmlEscape(data.items[0].snippet.title);
+                if (second) {
+                    var hours = Math.floor(second / 3600);
+                    var minutes = Math.floor(((second % 86400) % 3600) / 60);
+                    var seconds = ((second % 86400) % 3600) % 60;
+                    var durations = [];
+                    if (hours) {
+                        durations.push(hours + "h");
+                    }
+                    if (minutes) {
+                        durations.push(minutes + "m");
+                    }
+                    if (seconds) {
+                        durations.push(seconds + "s");
+                    }
+                    text += " <strong>[<i class=\"fa fa-clock-o\"></i>" + durations.join(" ") + "]</strong>";
+                }
                 deferred.resolve(genLink(prefix, link, text));
             } else {
                 deferred.resolve(genLink(prefix, link, linkText))
@@ -51,6 +67,7 @@ module.service("linkResolver", ["$q", "$http", function($q, $http) {
             var host = parsedUrl.host();
             if (host === "youtube.com" || host === "www.youtube.com") {
                 var videoId = null;
+                var second = parsedUrl.getQueryParamValue("t");
                 if (parsedUrl.getQueryParamValue("v")) {
                     videoId = parsedUrl.getQueryParamValue("v");
                 }
@@ -58,17 +75,18 @@ module.service("linkResolver", ["$q", "$http", function($q, $http) {
                     videoId = parsedUrl.getQueryParamValue("watch");
                 }
                 if (videoId) {
-                    fetchYoutubeTitle(videoId, ytKey, prefix, link, linkText, deferred);
+                    fetchYoutubeTitle(videoId, ytKey, prefix, link, linkText, second, deferred);
                 } else {
                     deferred.resolve(genLink(prefix, link, linkText));
                 }
             } else if (host === "youtu.be") {
                 var videoId = parsedUrl.uriParts.path;
+                var second = parsedUrl.getQueryParamValue("t");
                 if (videoId[0] === "/") {
                     videoId = videoId.substr(1);
                 }
                 if (videoId) {
-                    fetchYoutubeTitle(videoId, ytKey, prefix, link, linkText, deferred);
+                    fetchYoutubeTitle(videoId, ytKey, prefix, link, linkText, second, deferred);
                 } else {
                     deferred.resolve(genLink(prefix, link, linkText));
                 }
