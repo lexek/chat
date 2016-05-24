19e0c9a8baacbb8dda3bfc9e8f0941489f3b24ae
Belostotskiy Alexey <lexek92@mail.ru>
2016-05-24 22:25:54 +0300

    #258 deleted ConfirmEmailHandler

--- src/main/java/lexek/wschat/Main.java
+++ src/main/java/lexek/wschat/Main.java
@@ -388,7 +388,6 @@ public class Main {
         httpRequestDispatcher.add("/.*", new FileSystemStaticHandler(dataDir));
         httpRequestDispatcher.add("/.*", new ClassPathStaticHandler(ClassPathStaticHandler.class, "/static/"));
         httpRequestDispatcher.add("/chat.html", new ChatHomeHandler(coreSettings.getTitle(), settings.getHttp().isAllowLikes(), settings.getHttp().isSingleRoom()));
-        httpRequestDispatcher.add("/confirm_email", new ConfirmEmailHandler());
         httpRequestDispatcher.add("/recaptcha/[0-9]+", new RecaptchaHandler(captchaService, reCaptcha));
         httpRequestDispatcher.add("/api/tickets", new UserTicketsHandler(authenticationManager, ticketService));
         httpRequestDispatcher.add("/admin/.*", new AdminPageHandler(authenticationManager));
