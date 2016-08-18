a4b662b14388fcf8fc760f0d266101c52e2151d1
lexek <lexek-92@ya.ru>
2016-08-18 20:48:08 +0300

    #298 add contract annotation to listener

--- src/main/java/lexek/wschat/security/UserAuthEventListener.java
+++ src/main/java/lexek/wschat/security/UserAuthEventListener.java
@@ -1,7 +1,9 @@
 package lexek.wschat.security;
 
 import lexek.wschat.db.model.UserDto;
+import org.jvnet.hk2.annotations.Contract;
 
+@Contract
 public interface UserAuthEventListener {
     void onEvent(UserAuthEventType type, UserDto user, String service);
 }
