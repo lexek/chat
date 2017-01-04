a2072428e483a9a532615bbd10f969cb98d403a0
Belostotskiy Alexey <lexek92@mail.ru>
2017-01-04 05:23:53 +0300

    deprecating exception

--- src/main/java/lexek/wschat/chat/e/InvalidInputException.java
+++ src/main/java/lexek/wschat/chat/e/InvalidInputException.java
@@ -4,6 +4,9 @@ import com.google.common.collect.ImmutableMap;
 
 import java.util.Map;
 
+/**
+ * @deprecated
+ */
 public class InvalidInputException extends DomainException {
     private final String name;
     private final String message;
